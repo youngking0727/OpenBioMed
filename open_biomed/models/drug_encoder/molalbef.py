ca766e8ff81d68d2c1b9bcda32f8f9d2c1ed4f38
youngking0727 <youngking0727@126.com>
2023-05-16 10:13:07 +0000

    [feature]tmp commit 1. fix framework of dp 2. add feature of kg featurizer

--- open_biomed/models/drug_encoder/molalbef.py
+++ open_biomed/models/drug_encoder/molalbef.py
@@ -7,7 +7,7 @@ from models.drug_encoder.pyg_gnn import PygGNN
 from models.text_encoder.xbert import BertConfig, BertForMaskedLM
 from models.knowledge_encoder.transe import TransE
 
-from utils.mol_utils import convert_pyg_batch
+from utils.mol_utils import convert_pyg_batch, convert_kge_batch
 
 class MolALBEF(nn.Module):
     def __init__(self, config):
@@ -27,7 +27,7 @@ class MolALBEF(nn.Module):
             self.graph_encoder.load_state_dict(torch.load(config["gin_ckpt"]))
         self.graph_proj_head = nn.Linear(config["gin_hidden_dim"], config["projection_dim"])
         self.graph_linear = nn.Linear(config["gin_hidden_dim"], bert_config.hidden_size)
-        
+
         self.text_encoder = BertForMaskedLM(bert_config)
         if "bert_ckpt" in config:
             ckpt = torch.load(config["bert_ckpt"])
@@ -39,19 +39,31 @@ class MolALBEF(nn.Module):
                     else:
                         processed_ckpt[k] = v
             missing_keys, unexpected_keys = self.text_encoder.load_state_dict(processed_ckpt, strict=False)
-            print("missing keys:", missing_keys)
-            print("unexpected keys:", unexpected_keys)
+            #print("missing keys:", missing_keys)
+            #print("unexpected keys:", unexpected_keys)
         self.text_proj_head = nn.Linear(bert_config.hidden_size, config["projection_dim"])
         
         if "kge" in config:
             self.kg_encoder = TransE(**config["kge"])
-            #self.kg_proj_head = nn.Linear(config["kge"]["hidden_size"], config["projection_dim"])
+            self.kg_proj_head = nn.Linear(config["kge"]["hidden_size"], config["projection_dim"])
             self.kg_linear = nn.Linear(config["kge"]["hidden_size"], bert_config.hidden_size)
 
         self.mtm_head = nn.Linear(bert_config.hidden_size, 2)
 
+        self.temperature = 0.1
+        self.device = "cuda:0"
+        # TODO: 
+        self.text_linear = nn.Linear(bert_config.hidden_size, config["projection_dim"])
+        self.gin_hidden_dim = config["gin_hidden_dim"]
+        self.output_dim = config["gin_hidden_dim"] + config["projection_dim"]
+        # for test
+        self.output_dim = config["gin_hidden_dim"] + bert_config.hidden_size
+        #self.output_dim = config["gin_hidden_dim"]
+        
+
     def forward(self, mol, text, kg=None, cal_loss=False):
         mol_embeds, node_embeds = self.graph_encoder(mol)
+        # TODO: mol_feats?
         mol_feats = F.normalize(self.graph_proj_head(mol_embeds), dim=-1)
         all_node_feats = self.graph_linear(node_embeds)
         # serialize node feature
@@ -61,11 +73,20 @@ class MolALBEF(nn.Module):
 
         text_outputs = self.text_encoder.bert(text["input_ids"], attention_mask=text["attention_mask"], mode='text', return_dict=True)
         seq_feats = text_outputs["last_hidden_state"]
+        
         if kg is not None:
-            neigh_feats = self.kg_encoder.predict(kg["neigh_indice"])
-            neigh_feats = self.kg_linear(neigh_feats)
+            # TODO:
+            # neigh_feats = self.kg_encoder.predict(kg["neigh_indice"])
+            
+            all_neigh_feats = []
+            for i in kg:
+                neigh_feat = self.kg_encoder.predict(i)
+                neigh_feat = self.kg_linear(neigh_feat)
+                all_neigh_feats.append(neigh_feat)
+            neigh_feats, neigh_attention_mask = convert_kge_batch(all_neigh_feats)
+            # TODO:dropout text and kg embedding
             node_feats = torch.cat((node_feats, neigh_feats), dim=1)
-            node_attn = torch.cat((node_attn, kg["neigh_attn"]), dim=1)
+            node_attention_mask = torch.cat((node_attention_mask, neigh_attention_mask), dim=1)
         
         output = self.text_encoder.bert(
             encoder_embeds=seq_feats,
@@ -76,7 +97,7 @@ class MolALBEF(nn.Module):
             return_dict=True
         )
         if cal_loss:
-            perm =  []
+            perm = []
             for i in range(batch_size):
                 j = i
                 while j == i:
@@ -95,7 +116,7 @@ class MolALBEF(nn.Module):
             logits = self.mtm_head(torch.cat((output["last_hidden_state"][:, 0, :], output_neg["last_hidden_state"][:, 0, :]), dim=0))
             return F.cross_entropy(logits, label)
         else:
-            return output
+            return output, mol_embeds
 
     def encode_structure(self, structure, proj=True, return_node_feats=False):
         drug_embeds, node_embeds = self.graph_encoder(structure)
@@ -105,7 +126,43 @@ class MolALBEF(nn.Module):
             return drug_embeds
         else:
             return drug_embeds, node_embeds
-
+          
+    def encode_structure_with_all(self, mol, text, kg=None, cal_loss=False):
+        output, drug_embeds = self.forward(mol, text, kg=None, cal_loss=False)
+        text_embeds = F.dropout(output["last_hidden_state"][:,0,:], 0.5, training=self.training)
+        # text_embeds = self.text_linear(text_embeds)
+        # drug_embeds = self.graph_proj_head(drug_embeds)
+        return torch.cat((drug_embeds, text_embeds), dim=1)
+         
+    def encode_structure_with_kg(self, mol, kg, proj=True):
+        # get structure embedding first 
+        drug_embeds, _ = self.graph_encoder(mol)
+        kg_feats = self.kg_encoder.predict(kg)
+        if proj:
+            drug_embeds = self.graph_proj_head(drug_embeds)
+            kg_feats = self.kg_proj_head(kg_feats)
+        feats = torch.cat((drug_embeds, kg_feats.squeeze()), dim=1)
+        return feats
+    
+    def encode_all_module(self, mol, kg, text, proj=True):
+        # get structure embedding first 
+        # TODO:
+        self.output_dim = 256 * 3
+        drug_embeds, _ = self.graph_encoder(mol)
+        kg_feats = []
+        for i in kg:
+            kg_feat = self.kg_encoder.predict(i).mean(0)
+            kg_feats.append(kg_feat)
+        kg_feats = torch.cat([i.unsqueeze(0) for i in kg_feats], 0)
+        text_embeds = self.text_encoder.bert(text["input_ids"], attention_mask=text["attention_mask"], mode='text', return_dict=True)["last_hidden_state"]
+        if proj:
+            drug_embeds = self.graph_proj_head(drug_embeds)
+            kg_feats = self.kg_proj_head(kg_feats)
+            text_embeds = self.text_proj_head(text_embeds)
+        feats = torch.cat((drug_embeds, kg_feats, text_embeds[:, 0, :].squeeze()), dim=1)
+        return feats
+    
+    
     def encode_structure_with_prob(self, structure, x, atomic_num_list, device):
         drug_embeds, _ = self.graph_encoder(structure, x, atomic_num_list, device)
         return self.graph_proj_head(drug_embeds) 
@@ -120,7 +177,7 @@ class MolALBEF(nn.Module):
             return text_embeds
 
     def encode_knowledge(self, kg):
-        return self.predict(kg)
+        return self.kg_encoder.predict(kg)
 
     def predict_similarity_score(self, data):
         preds = self.forward(data["structure"], data["text"])["last_hidden_state"][:, 0, :]
--- open_biomed/scripts/dp/train_deepeik.sh
+++ open_biomed/scripts/dp/train_deepeik.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-DATASET="Tox21"
+# BBBP, Tox21, Toxcast, sider, clintox, muv, hiv, bace
 
-python tasks/mol_task/dp.py \
+DATASET="Tox21"
+for seed in 0 1 2
+do
+python tasks/mol_task/dp_mvp_3.py \
 --device cuda:0 \
 --dataset MoleculeNet \
 --dataset_path ../datasets/dp/moleculenet \
 --dataset_name $DATASET \
 --config_path ./configs/dp/deepeik.json \
---output_path ../ckpts/finetune_ckpts/dp/deepeik_finetune_$DATASET.pth \
+--output_path ../ckpts/finetune_ckpts/dp \
 --num_workers 1 \
 --mode train \
---batch_size 256 \
+--batch_size 128 \
 --epochs 80 \
---patience 20
+--weight_decay 1e-4 \
+--lr 1e-4 \
+--patience 20 \
+--dropout 0.2 \
+--seed $seed
+--freeze
+done
+
 
